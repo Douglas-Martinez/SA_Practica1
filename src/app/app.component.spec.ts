5dadd05f8ba9345315576abd1b86dea81d8855ea
douglas_martinez <douglasmartinez.ecys@gmail.com>
2021-06-06 23:56:38 -0600

    fallo test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'practica1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('practica1');
+    expect(app.title).toEqual('Otro titulo');
   });
 
   it('should render title', () => {
